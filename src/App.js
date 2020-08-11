1928c586adb7c8719970e122826675413becaab9
sara <saraahmed19594@gmail.com>
2020-08-11 14:13:00 +0200

    pull request

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div>
-        <h2>sara ahmed</h2>
-        <p>frond end developer</p>
+class App extends Component {
+  mooncakeSays(message) {
+    let mooncake = "(O.o)";
+    if (!message) {
+      return `${mooncake} chookty?`;
+    }
+    return `${mooncake} ${message}`;
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("mooncake knows what to say", () => {
+  expect(mooncakeSays("wooahh")).toBe("(o.o) wooahh");
+});
+
+test("mooncake is not sure what to say", () => {
+  expect(mooncakeSays()).toBe("(o.o) chookty?");
 });
